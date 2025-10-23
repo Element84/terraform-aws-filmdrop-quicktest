ecdfc7f577763612fd1b242000d1d25fedcf06ff
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2025-10-23 16:43:33 +0000

    setup stacserver

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  project_ident = "a1b2"
+  project_ident = "stcv"
 }
 
 module "filmdrop" {
@@ -32,10 +32,11 @@ module "filmdrop" {
 
   # Infrastructure flags
   deploy_vpc_search                        = true
+  deploy_log_archive                       = true # required by numerous modules, recommend leaving as true
   deploy_cirrus                            = false
   deploy_cirrus_dashboard                  = false
   deploy_stac_server_opensearch_serverless = false
-  deploy_stac_server                       = false
+  deploy_stac_server                       = true
   deploy_stac_server_outside_vpc           = false
   deploy_titiler                           = false
   deploy_console_ui                        = false
@@ -43,11 +44,9 @@ module "filmdrop" {
   deploy_waf_rule                          = false
   deploy_analytics                         = false
   deploy_vpc                               = false
-  deploy_log_archive                       = false
 
+  #region Cirrus Testing
   # ==============================
-  # Cirrus Testing
-  #
   # To test Cirrus Workflows:
   # - Set deploy_cirrus = true above
   # - Uncomment cirrus_inputs below. Selectively uncomment the _definitions_dirs you want to use.
@@ -110,4 +109,76 @@ module "filmdrop" {
   #     memory  = 512
   #   }
   # }
+  #endregion
+
+  #region stac-server Testing
+  # ==============================
+  # To test stac-server:
+  # - Set deploy_cirrus = true above
+  # - Uncomment stac_server_inputs below
+  # ==============================
+  stac_server_inputs = {
+    app_name                                    = "stac_server"
+    version                                     = "v3.10.0"
+    stac_id                                     = "stac-server"
+    stac_title                                  = "STAC API"
+    stac_title                                  = "A STAC API using stac-server"
+    api_rest_type                               = "EDGE"
+    api_method_authorization_type               = "NONE"
+    deploy_cloudfront                           = true
+    web_acl_id                                  = ""
+    domain_alias                                = ""
+    enable_transactions_extension               = false
+    enable_collections_authx                    = false
+    enable_filter_authx                         = false
+    enable_response_compression                 = true
+    items_max_limit                             = 100
+    enable_ingest_action_truncate               = false
+    collection_to_index_mappings                = ""
+    opensearch_version                          = "OpenSearch_2.17"
+    opensearch_cluster_instance_type            = "t3.small.search"
+    opensearch_cluster_instance_count           = 3
+    opensearch_cluster_dedicated_master_enabled = true
+    opensearch_cluster_dedicated_master_type    = "t3.small.search"
+    opensearch_cluster_dedicated_master_count   = 3
+    opensearch_cluster_availability_zone_count  = 3
+    opensearch_ebs_volume_size                  = 35
+    ingest_sns_topic_arns                       = []
+    additional_ingest_sqs_senders_arns          = []
+    cors_origin                                 = "*"
+    cors_credentials                            = false
+    cors_methods                                = ""
+    cors_headers                                = ""
+    authorized_s3_arns                          = []
+    private_api_additional_security_group_ids   = null
+    api_lambda                                  = null
+    ingest_lambda                               = null
+    pre_hook_lambda                             = null
+    private_certificate_arn                     = ""
+    vpce_private_dns_enabled                    = false
+    auth_function = {
+      cf_function_name             = ""
+      cf_function_runtime          = "cloudfront-js-2.0"
+      cf_function_code_path        = ""
+      attach_cf_function           = false
+      cf_function_event_type       = "viewer-request"
+      create_cf_function           = false
+      create_cf_basicauth_function = false
+      cf_function_arn              = ""
+    }
+    ingest = {
+      source_catalog_url               = ""
+      destination_collections_list     = ""
+      destination_collections_min_lat  = -90
+      destination_collections_min_long = -180
+      destination_collections_max_lat  = 90
+      destination_collections_max_long = 180
+      date_start                       = ""
+      date_end                         = ""
+      include_historical_ingest        = false
+      source_sns_arn                   = ""
+      include_ongoing_ingest           = false
+    }
+  }
+  #endregion
 }
